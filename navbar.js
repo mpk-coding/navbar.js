186172f4e22f9a287cee50f94b2340e80c442e2e
mpk-coding <thamak@gmail.com>
2022-02-04 01:10:50 +0100

    supportedEvents array, linked eventListener and handlers to it thus adding an easy way to manage events; Refactor

--- navbar.js
+++ navbar.js
@@ -1,122 +1,76 @@
 const navBar = () => {
   console.log("===\nnavbar.js imported!");
+  // preferably configurable and flexible in the future (use of .contains() instead of .parentElement)
   const nav = document.querySelector("header > nav");
   const navList = nav.querySelector(".nav__list");
   const dropdowns = document.querySelectorAll("header .nav__dropdown");
-  //
-  // DEFS
-  //
-  const onScrollSticky = () => {
-    //
-  };
-  //
+  const supportedEvents = [
+    {
+      event: "click",
+      handler: (event) => {
+        const isDropdown = Array.from(dropdowns).find((dropdown) => {
+          return event.target === dropdown.parentElement;
+        });
+        if (isDropdown) {
+          closeCurrentLevelDropdowns(event);
+          isDropdown.classList.toggle("show");
+        }
+      },
+    },
+    {
+      event: "mouseover",
+      handler: (event) => {
+        closeCurrentLevelDropdowns(event);
+      },
+    },
+    {
+      event: "mouseleave",
+      handler: (event) => {
+        closeAllDropdowns(event);
+      },
+    },
+  ];
   const addDropdownCarets = () => {
-    //
     dropdowns.forEach((dropdown) => {
       const caret = document.createElement("span");
       const caretContent = document.createTextNode("^");
       caret.setAttribute("class", "nav__caret");
       caret.append(caretContent);
       dropdown.parentElement.append(caret);
-      //
     });
-    //
   };
-  //
+  const closeCurrentLevelDropdowns = (event) => {
+    const root =
+      event.target.parentElement === navList
+        ? navList
+        : event.target.parentElement;
+    const dropdowns = root.querySelectorAll(".nav__dropdown");
+    Array.from(dropdowns).find((dropdown) => {
+      return (
+        dropdown.parentElement !== event.target &&
+        dropdown.classList.remove("show")
+      );
+    });
+  };
+  const closeAllDropdowns = (event) => {
+    Array.from(dropdowns).map((dropdown) => {
+      return dropdown.classList.remove("show");
+    });
+  };
   const dropdownHandler = (event) => {
-    //
-    const closeCurrentLevelDropdowns = (event) => {
-      const root =
-        event.target.parentElement === navList
-          ? navList
-          : event.target.parentElement;
-      //
-      const dropdowns = root.querySelectorAll(".nav__dropdown");
-      //
-      dropdowns.forEach((dropdown) => {
-        //  close every dropdown except for the event.target relevant one
-        //  so as not to close the one that is supposed to be opened
-        if (dropdown.parentElement !== event.target) {
-          dropdown.classList.remove("show");
-        }
-        //
-      });
-      //
-    };
-    //
-    const closeAllDropdowns = (event) => {
-      dropdowns.forEach((dropdown) => {
-        dropdown.classList.remove("show");
-        //
-      });
-      //
-    };
-    //
-    const clickHandler = (event) => {
-      const isDropdown = Array.from(dropdowns).find((dropdown) => {
-        return event.target === dropdown.parentElement;
-        //
-      });
-      //
-      if (isDropdown) {
-        closeCurrentLevelDropdowns(event);
-        //
-        isDropdown.classList.toggle("show");
-        //
-      }
-      //
-    };
-    //
-    const mouseoverHandler = (event) => {
-      closeCurrentLevelDropdowns(event);
-      //
-    };
-    //
-    const mouseleaveHandler = (event) => {
-      closeAllDropdowns(event);
-      //
-    };
-    //
-    if (event.type === "click") {
-      clickHandler(event);
-      //
-    }
-    //
-    if (event.type === "mouseover") {
-      mouseoverHandler(event);
-      //
-    }
-    //
-    if (event.type === "mouseleave") {
-      mouseleaveHandler(event);
-      //
-    }
-    //
+    supportedEvents.find((triggeredEvent) => {
+      return (
+        triggeredEvent.event === event.type && triggeredEvent.handler(event)
+      );
+    });
   };
-  //
   const initNav = () => {
-    //
     addDropdownCarets();
-    //
-    nav.addEventListener("click", (event) => {
-      dropdownHandler(event);
-      //
+    supportedEvents.map((supportedEvent) => {
+      return nav.addEventListener(supportedEvent["event"], dropdownHandler);
     });
-    //
-    nav.addEventListener("mouseover", (event) => {
-      dropdownHandler(event);
-      //
-    });
-    //
-    nav.addEventListener("mouseleave", (event) => {
-      dropdownHandler(event);
-      //
-    });
-    //
   };
-  //
-  initNav();
-  //
+  return initNav();
 };
 
 window.onload = (event) => {
